eb8bbcceaaec513f176940b4c0d89fbc521968de
Christian Schneider <mail@christian-schneider.net>
2020-08-04 17:55:19 +0200

    repo links

--- main.go
+++ main.go
@@ -3835,8 +3835,8 @@ func printLogo() {
 
 func printVersion() {
 	fmt.Println("Documentation: https://threagile.io")
-	fmt.Println("Docker Images: https://hub.docker.com/orgs/threagile")
-	fmt.Println("Source Code: https://github.com/threagile")
+	fmt.Println("Docker Images: https://hub.docker.com/r/threagile")
+	fmt.Println("Sourcecode: https://github.com/threagile")
 	fmt.Println("License: Open-Source (MIT License)")
 	fmt.Println("Version: " + model.ThreagileVersion + " (" + buildTimestamp + ")")
 	fmt.Println()
