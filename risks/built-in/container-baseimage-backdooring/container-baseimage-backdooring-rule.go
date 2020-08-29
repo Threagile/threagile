c022d4e468023123b0e4b281cae375909d689af6
Christian Schneider <mail@christian-schneider.net>
2020-08-29 17:28:59 +0200

    Wording

--- risks/built-in/container-baseimage-backdooring/container-baseimage-backdooring-rule.go
+++ risks/built-in/container-baseimage-backdooring/container-baseimage-backdooring-rule.go
@@ -7,23 +7,23 @@ import (
 func Category() model.RiskCategory {
 	return model.RiskCategory{
 		Id:    "container-baseimage-backdooring",
-		Title: "Container Baseimage Backdooring",
-		Description: "When a technical asset is built using container technologies, Baseimage Backdooring risks might arise where " +
-			"baseimages and other layers used contain malicious backdoors." +
+		Title: "Container Base Image Backdooring",
+		Description: "When a technical asset is built using container technologies, Base Image Backdooring risks might arise where " +
+			"base images and other layers used contain malicious backdoors." +
 			"<br><br>See for example: <a href=\"https://techcrunch.com/2018/06/15/tainted-crypto-mining-containers-pulled-from-docker-hub/\">https://techcrunch.com/2018/06/15/tainted-crypto-mining-containers-pulled-from-docker-hub/</a>",
 		Impact:     "If this risk is unmitigated, attackers might be able to deeply persist in the target system by executing code in deployed containers.",
 		ASVS:       "V10 - Malicious Code Verification Requirements",
 		CheatSheet: "https://cheatsheetseries.owasp.org/cheatsheets/Docker_Security_Cheat_Sheet.html",
 		Action:     "Container Infrastructure Hardening",
 		Mitigation: "Apply hardening of all container infrastructures (see for example the <i>CIS-Benchmarks for Docker and Kubernetes</i> and the <i>Docker Bench for Security</i>). " +
-			"Use only trusted baseimages of the original vendors, verify digital signatures and apply image creation best practices. " +
+			"Use only trusted base images of the original vendors, verify digital signatures and apply image creation best practices. " +
 			"Regularly execute container image vulnerability scans with tools checking the layers for known vulnerable components.",
 		Check:          "Are recommendations from the linked cheat sheet and referenced ASVS or CSVS chapter applied?",
 		Function:       model.Operations,
 		STRIDE:         model.Tampering,
 		DetectionLogic: "In-scope technical assets running as containers.",
 		RiskAssessment: "The risk rating depends on the sensitivity of the technical asset itself and of the data assets processed and stored.",
-		FalsePositives: "Fully trusted (i.e. reviewed and cryptographically signed or similar) baseimages of containers can be considered " +
+		FalsePositives: "Fully trusted (i.e. reviewed and cryptographically signed or similar) base images of containers can be considered " +
 			"as false positives after individual review.",
 		ModelFailurePossibleReason: false,
 		CWE:                        912,
@@ -46,7 +46,7 @@ func GenerateRisks() []model.Risk {
 }
 
 func createRisk(technicalAsset model.TechnicalAsset) model.Risk {
-	title := "<b>Container Baseimage Backdooring</b> risk at <b>" + technicalAsset.Title + "</b>"
+	title := "<b>Container Base Image Backdooring</b> risk at <b>" + technicalAsset.Title + "</b>"
 	impact := model.MediumImpact
 	if technicalAsset.HighestConfidentiality() == model.StrictlyConfidential ||
 		technicalAsset.HighestIntegrity() == model.MissionCritical ||
