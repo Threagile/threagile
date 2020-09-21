8d6f28c62335f8f4aec0ccd44db54c233518ba84
Christian Schneider <mail@christian-schneider.net>
2020-09-21 22:35:15 +0200

    Formatting

--- risks/built-in/container-baseimage-backdooring/container-baseimage-backdooring-rule.go
+++ risks/built-in/container-baseimage-backdooring/container-baseimage-backdooring-rule.go
@@ -17,7 +17,7 @@ func Category() model.RiskCategory {
 		Action:     "Container Infrastructure Hardening",
 		Mitigation: "Apply hardening of all container infrastructures (see for example the <i>CIS-Benchmarks for Docker and Kubernetes</i> and the <i>Docker Bench for Security</i>). " +
 			"Use only trusted base images of the original vendors, verify digital signatures and apply image creation best practices. " +
-			"Also consider using Google's <b>Distroless</i> base images or otherwise very small base images. " +
+			"Also consider using Google's <i>Distroless</i> base images or otherwise very small base images. " +
 			"Regularly execute container image vulnerability scans with tools checking the layers for known vulnerable components.",
 		Check:          "Are recommendations from the linked cheat sheet and referenced ASVS or CSVS chapter applied?",
 		Function:       model.Operations,
