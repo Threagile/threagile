261b0a16208194935fa6825f100d8d06134cdb84
Yevhen Zavhorodnii <yevhen.zavhorodnii@ticketmaster.co.uk>
2023-12-22 15:10:13 +0000

    Fix comment

--- cmd/threagile/main.go
+++ cmd/threagile/main.go
@@ -11,9 +11,8 @@ const (
 // === Error handling stuff ========================================
 
 func main() {
-
 	// TODO: uncomment below as soon as refactoring is finished - everything will go through rootCmd.Execute
-	// cmd.Execute()
+	// threagile.Execute()
 
 	// TODO: remove below as soon as refactoring is finished - everything will go through rootCmd.Execute
 	// for now it's fine to have as frequently uncommented to see the actual behaviour
