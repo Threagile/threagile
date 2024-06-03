156eddda09fc875e6f21bfc5905678776c40cc20
Yevhen Zavhorodnii <yevhen.zavhorodnii@ticketmaster.co.uk>
2024-06-03 11:49:35 +0100

    Refactor to reduce nesting

--- pkg/security/risks/builtin/unchecked_deployment_rule.go
+++ pkg/security/risks/builtin/unchecked_deployment_rule.go
@@ -59,20 +59,21 @@ func (r *UncheckedDeploymentRule) createRisk(input *types.Model, technicalAsset 
 	uniqueDataBreachTechnicalAssetIDs := make(map[string]interface{})
 	uniqueDataBreachTechnicalAssetIDs[technicalAsset.Id] = true
 	for _, codeDeploymentTargetCommLink := range technicalAsset.CommunicationLinks {
-		if codeDeploymentTargetCommLink.Usage == types.DevOps {
-			for _, dataAssetID := range codeDeploymentTargetCommLink.DataAssetsSent {
-				// it appears to be code when elevated integrity rating of sent data asset
-				if input.DataAssets[dataAssetID].Integrity >= types.Important {
-					// here we've got a deployment target which has its data assets at risk via deployment of backdoored code
-					uniqueDataBreachTechnicalAssetIDs[codeDeploymentTargetCommLink.TargetId] = true
-					targetTechAsset := input.TechnicalAssets[codeDeploymentTargetCommLink.TargetId]
-					if targetTechAsset.HighestProcessedConfidentiality(input) >= types.Confidential ||
-						targetTechAsset.HighestProcessedIntegrity(input) >= types.Critical ||
-						targetTechAsset.HighestProcessedAvailability(input) >= types.Critical {
-						impact = types.MediumImpact
-					}
-					break
+		if codeDeploymentTargetCommLink.Usage != types.DevOps {
+			continue
+		}
+		for _, dataAssetID := range codeDeploymentTargetCommLink.DataAssetsSent {
+			// it appears to be code when elevated integrity rating of sent data asset
+			if input.DataAssets[dataAssetID].Integrity >= types.Important {
+				// here we've got a deployment target which has its data assets at risk via deployment of backdoored code
+				uniqueDataBreachTechnicalAssetIDs[codeDeploymentTargetCommLink.TargetId] = true
+				targetTechAsset := input.TechnicalAssets[codeDeploymentTargetCommLink.TargetId]
+				if targetTechAsset.HighestProcessedConfidentiality(input) >= types.Confidential ||
+					targetTechAsset.HighestProcessedIntegrity(input) >= types.Critical ||
+					targetTechAsset.HighestProcessedAvailability(input) >= types.Critical {
+					impact = types.MediumImpact
 				}
+				break
 			}
 		}
 	}
