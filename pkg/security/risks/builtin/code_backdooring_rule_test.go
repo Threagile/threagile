d59a978223707b919d67bb79edbf3f0d5d848bec
Yevhen Zavhorodnii <yevhen.zavhorodnii@ticketmaster.co.uk>
2024-06-05 10:47:17 +0100

    Better code coverage for code backdooring rule

--- pkg/security/risks/builtin/code_backdooring_rule_test.go
+++ pkg/security/risks/builtin/code_backdooring_rule_test.go
@@ -224,3 +224,177 @@ func TestCodeBackdooringRuleGenerateRisksTechAssetNotInternetButComingThroughVPN
 	assert.Nil(t, err)
 	assert.Empty(t, risks)
 }
+
+func TestCodeBackdoogingRuleGenerateRisksNotImportantDataAssetNoAddingTargetRisksCreated(t *testing.T) {
+	rule := NewCodeBackdooringRule()
+
+	risks, err := rule.GenerateRisks(&types.Model{
+		TechnicalAssets: map[string]*types.TechnicalAsset{
+			"git-lab-ci-cd": {
+				Id:    "git-lab-ci-cd",
+				Title: "GitLab CI/CD",
+				Technologies: types.TechnologyList{
+					{
+						Name: "build-pipeline",
+						Attributes: map[string]bool{
+							types.IsDevelopmentRelevant: true,
+						},
+					},
+				},
+				CommunicationLinks: []*types.CommunicationLink{
+					{
+						Usage:          types.DevOps,
+						DataAssetsSent: []string{"not-important-data-asset"},
+						TargetId:       "deployment-target",
+						SourceId:       "git-lab-ci-cd",
+					},
+				},
+			},
+			"reverse-proxy": {
+				Id:       "reverse-proxy",
+				Title:    "Reverse Proxy",
+				Internet: true,
+			},
+			"deployment-target": {
+				Id:    "deployment-target",
+				Title: "Deployment Target",
+			},
+		},
+		IncomingTechnicalCommunicationLinksMappedByTargetId: map[string][]*types.CommunicationLink{
+			"git-lab-ci-cd": {
+				{
+					SourceId: "reverse-proxy",
+					TargetId: "git-lab-ci-cd",
+					VPN:      false,
+				},
+			},
+		},
+		DataAssets: map[string]*types.DataAsset{
+			"not-important-data-asset": {
+				Integrity: types.Operational,
+			},
+		},
+	})
+
+	assert.Nil(t, err)
+	assert.NotEmpty(t, risks)
+	assert.Equal(t, "<b>Code Backdooring</b> risk at <b>GitLab CI/CD</b>", risks[0].Title)
+	assert.Equal(t, types.MediumImpact, risks[0].ExploitationImpact)
+	assert.Equal(t, []string{"git-lab-ci-cd"}, risks[0].DataBreachTechnicalAssetIDs)
+}
+
+func TestCodeBackdoogingRuleGenerateRisksNotDevOpsDataAssetNoAddingTargetRisksCreated(t *testing.T) {
+	rule := NewCodeBackdooringRule()
+
+	risks, err := rule.GenerateRisks(&types.Model{
+		TechnicalAssets: map[string]*types.TechnicalAsset{
+			"git-lab-ci-cd": {
+				Id:    "git-lab-ci-cd",
+				Title: "GitLab CI/CD",
+				Technologies: types.TechnologyList{
+					{
+						Name: "build-pipeline",
+						Attributes: map[string]bool{
+							types.IsDevelopmentRelevant: true,
+						},
+					},
+				},
+				CommunicationLinks: []*types.CommunicationLink{
+					{
+						Usage:          types.Business,
+						DataAssetsSent: []string{"important-data-asset"},
+						TargetId:       "deployment-target",
+						SourceId:       "git-lab-ci-cd",
+					},
+				},
+			},
+			"reverse-proxy": {
+				Id:       "reverse-proxy",
+				Title:    "Reverse Proxy",
+				Internet: true,
+			},
+			"deployment-target": {
+				Id:    "deployment-target",
+				Title: "Deployment Target",
+			},
+		},
+		IncomingTechnicalCommunicationLinksMappedByTargetId: map[string][]*types.CommunicationLink{
+			"git-lab-ci-cd": {
+				{
+					SourceId: "reverse-proxy",
+					TargetId: "git-lab-ci-cd",
+					VPN:      false,
+				},
+			},
+		},
+		DataAssets: map[string]*types.DataAsset{
+			"important-data-asset": {
+				Integrity: types.Important,
+			},
+		},
+	})
+
+	assert.Nil(t, err)
+	assert.NotEmpty(t, risks)
+	assert.Equal(t, "<b>Code Backdooring</b> risk at <b>GitLab CI/CD</b>", risks[0].Title)
+	assert.Equal(t, types.MediumImpact, risks[0].ExploitationImpact)
+	assert.Equal(t, []string{"git-lab-ci-cd"}, risks[0].DataBreachTechnicalAssetIDs)
+}
+
+func TestCodeBackdoogingRuleGenerateRisksDevOpsImportantDataAssetNoAddingTargetRisksCreated(t *testing.T) {
+	rule := NewCodeBackdooringRule()
+
+	risks, err := rule.GenerateRisks(&types.Model{
+		TechnicalAssets: map[string]*types.TechnicalAsset{
+			"git-lab-ci-cd": {
+				Id:    "git-lab-ci-cd",
+				Title: "GitLab CI/CD",
+				Technologies: types.TechnologyList{
+					{
+						Name: "build-pipeline",
+						Attributes: map[string]bool{
+							types.IsDevelopmentRelevant: true,
+						},
+					},
+				},
+				CommunicationLinks: []*types.CommunicationLink{
+					{
+						Usage:          types.DevOps,
+						DataAssetsSent: []string{"important-data-asset"},
+						TargetId:       "deployment-target",
+						SourceId:       "git-lab-ci-cd",
+					},
+				},
+			},
+			"reverse-proxy": {
+				Id:       "reverse-proxy",
+				Title:    "Reverse Proxy",
+				Internet: true,
+			},
+			"deployment-target": {
+				Id:    "deployment-target",
+				Title: "Deployment Target",
+			},
+		},
+		IncomingTechnicalCommunicationLinksMappedByTargetId: map[string][]*types.CommunicationLink{
+			"git-lab-ci-cd": {
+				{
+					SourceId: "reverse-proxy",
+					TargetId: "git-lab-ci-cd",
+					VPN:      false,
+				},
+			},
+		},
+		DataAssets: map[string]*types.DataAsset{
+			"important-data-asset": {
+				Integrity: types.Important,
+			},
+		},
+	})
+
+	assert.Nil(t, err)
+	assert.NotEmpty(t, risks)
+	assert.Equal(t, "<b>Code Backdooring</b> risk at <b>GitLab CI/CD</b>", risks[0].Title)
+	assert.Equal(t, types.MediumImpact, risks[0].ExploitationImpact)
+	assert.Equal(t, []string{"git-lab-ci-cd", "deployment-target"}, risks[0].DataBreachTechnicalAssetIDs)
+}
