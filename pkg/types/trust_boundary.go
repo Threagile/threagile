b7492b2972d3bb53e3ae13053b237db5f15004fa
Yevhen Zavhorodnii <yevhen.zavhorodnii@ticketmaster.co.uk>
2024-06-07 20:50:54 +0100

    Add forgotten check

--- pkg/types/trust_boundary.go
+++ pkg/types/trust_boundary.go
@@ -14,33 +14,6 @@ type TrustBoundary struct {
 	TrustBoundariesNested []string          `json:"trust_boundaries_nested,omitempty" yaml:"trust_boundaries_nested,omitempty"`
 }
 
-func (what TrustBoundary) RecursivelyAllTechnicalAssetIDsInside(model *Model) []string {
-	result := make([]string, 0)
-	what.addAssetIDsRecursively(model, &result)
-	return result
-}
-
 func (what TrustBoundary) IsTaggedWithAny(tags ...string) bool {
 	return containsCaseInsensitiveAny(what.Tags, tags...)
-}
-
-func (what TrustBoundary) AllParentTrustBoundaryIDs(model *Model) []string {
-	result := make([]string, 0)
-	what.addTrustBoundaryIDsRecursively(model, &result)
-	return result
-}
-
-func (what TrustBoundary) addAssetIDsRecursively(model *Model, result *[]string) {
-	*result = append(*result, what.TechnicalAssetsInside...)
-	for _, nestedBoundaryID := range what.TrustBoundariesNested {
-		model.TrustBoundaries[nestedBoundaryID].addAssetIDsRecursively(model, result)
-	}
-}
-
-func (what TrustBoundary) addTrustBoundaryIDsRecursively(model *Model, result *[]string) {
-	*result = append(*result, what.Id)
-	parent := model.FindParentTrustBoundary(&what)
-	if parent != nil {
-		parent.addTrustBoundaryIDsRecursively(model, result)
-	}
 }
