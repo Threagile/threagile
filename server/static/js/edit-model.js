a1b62c4f6e5946c100dcd58304685b689afbab5d
Yevhen Zavhorodnii <yevhen.zavhorodnii@ticketmaster.co.uk>
2024-11-15 09:45:32 +0000

    Use less vanilla JS and more jQuery

--- server/static/js/edit-model.js
+++ server/static/js/edit-model.js
@@ -3,151 +3,152 @@ var myDiagram;
 var currentFile;
 var diagramYaml;
 
-document.getElementById('fileInput').addEventListener('change', function(event) {
-  const file = event.target.files[0];
-  if (file) {
-      const reader = new FileReader();
-      reader.onload = function(e) {
-          try {
-              currentFile = e.target.result;
+$(document).ready(function() {
+  myDiagram = new go.Diagram('myDiagramDiv');
+  myDiagram.layout = new go.LayeredDigraphLayout({
+    layerSpacing: 50,
+    setsPortSpots: false
+  });
 
-              const yamlData = jsyaml.load(e.target.result);
-              console.log(yamlData);
-              updateDiagramModel(yamlData, document.getElementById('showDataAssetsCheckBox').checked);
-          } catch (error) {
-              console.error('Error parsing YAML:', error);
-              alert('Failed to parse YAML file.');
-          }
-      };
-      reader.readAsText(file);
-  }
-});
+  myDiagram.nodeTemplate = new go.Node('Auto', {
+    click: nodeClicked,
+  })
+    .add(
+      new go.Shape({ name: 'SHAPE', figure: 'RoundedRectangle', fill: 'lightgray' }).bind('fill', 'color')
+    )
+    .add(
+      new go.TextBlock({ margin: 2, textAlign: 'center' }).bind('text', 'caption')
+    );
 
-document.getElementById('showDataAssetsCheckBox').addEventListener('change', function(event) {
-  restoreChanges();
-});
 
-function nodeClicked(e, obj) {
-  var evt = e.copy();
-  var node = obj.part;
-  var type = evt.clickCount === 2 ? 'Double-Clicked: ' : 'Clicked: ';
-  console.log(type + 'on ' + node);
-  if (evt.clickCount === 2) {
-    var editorSchema = node.data.type === 'data_asset' ?
-                          schema.properties.data_assets.additionalProperties.properties :
-                          schema.properties.technical_assets.additionalProperties.properties;
-    openPropertyEditor(node.data.threagile_model, 'itemPropertyEditor', editorSchema);
-  }
-}
+  $('#fileInput').on('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            try {
+                currentFile = e.target.result;
 
-function init() {
-    myDiagram = new go.Diagram('myDiagramDiv');
-    myDiagram.layout = new go.LayeredDigraphLayout({
-      layerSpacing: 50,
-      setsPortSpots: false
-    });
+                const yamlData = jsyaml.load(e.target.result);
+                console.log(yamlData);
 
-    myDiagram.nodeTemplate = new go.Node('Auto', {
-      click: nodeClicked,
-    })
-      .add(
-        new go.Shape({ name: 'SHAPE', figure: 'RoundedRectangle', fill: 'lightgray' }).bind('fill', 'color')
-      )
-      .add(
-        new go.TextBlock({ margin: 2, textAlign: 'center' }).bind('text', 'caption')
-      );
-}
+                const showDataAssets = $('#showDataAssetsCheckBox').is(':checked');
+                updateDiagramModel(yamlData, showDataAssets);
+            } catch (error) {
+                console.error('Error parsing YAML:', error);
+                alert('Failed to parse YAML file.');
+            }
+        };
+        reader.readAsText(file);
+    }
+  });
 
-function updateDiagramModel(yamlData, showDataAssets) {
-  diagramYaml = yamlData;
+  $('#showDataAssetsCheckBox').on('change', function() {
+    restoreChanges();
+  });
 
-  let nodeDataArray = [];
-  if (showDataAssets) {
-    for (const daKey in yamlData.data_assets) {
-      if (yamlData.data_assets.hasOwnProperty(daKey)) {
-        const data_asset = yamlData.data_assets[daKey];
-        console.log(`${daKey}: ${data_asset}`);
-        nodeDataArray.push({ key: data_asset.id, threagile_model: data_asset, type: 'data_asset', caption: daKey, color: 'lightgreen' });
-      }
+  $('#btnRestoreChanges').on('click', function() {
+    if (currentFile) {
+      const yamlData = jsyaml.load(currentFile);
+      console.log(yamlData);
+      updateDiagramModel(yamlData, $('#showDataAssetsCheckBox').is(':checked'));
+    }
+  });
+
+  $('#btnExportDiagram').on('click', function() {
+    try {
+      const yamlData = jsyaml.dump(diagramYaml);
+      const blob = new Blob([yamlData], { type: 'text/yaml' });
+      const downloadLink = document.createElement('a');
+      downloadLink.href = URL.createObjectURL(blob);
+      downloadLink.download = 'diagram.yaml'; // Default file name
+      downloadLink.click();
+      URL.revokeObjectURL(downloadLink.href);
+    } catch (e) {
+      alert('Failed to export diagram.');
+      console.error("Error exporting diagram:", e);
+    }
+  });
+
+  function nodeClicked(e, obj) {
+    var evt = e.copy();
+    var node = obj.part;
+    var type = evt.clickCount === 2 ? 'Double-Clicked: ' : 'Clicked: ';
+    console.log(type + 'on ' + node);
+    if (evt.clickCount === 2) {
+      var editorSchema = node.data.type === 'data_asset' ?
+                            schema.properties.data_assets.additionalProperties.properties :
+                            schema.properties.technical_assets.additionalProperties.properties;
+      openPropertyEditor(node.data.threagile_model, 'itemPropertyEditor', editorSchema);
     }
   }
 
-  let nodesLinks = [];
-  for (const taKey in yamlData.technical_assets) {
-    if (yamlData.technical_assets.hasOwnProperty(taKey)) {
-      const technical_asset = yamlData.technical_assets[taKey];
-      console.log(`${taKey}: ${technical_asset}`);
-      nodeDataArray.push({ key: technical_asset.id, threagile_model: technical_asset, type: 'technical_asset', caption: taKey, color: 'lightblue' });
+  function updateDiagramModel(yamlData, showDataAssets) {
+    diagramYaml = yamlData;
 
-      if (technical_asset.communication_links) {
-        for (const clKey in technical_asset.communication_links) {
-          const communicationLink = technical_asset.communication_links[clKey];
-          console.log(`${clKey}: ${communicationLink}`);
-          nodesLinks.push({ from: technical_asset.id, to: communicationLink.target });
-
-          if (showDataAssets && communicationLink.data_assets_sent) {
-            communicationLink.data_assets_sent.forEach((dataAsset) => {
-              nodesLinks.push({ from: technical_asset.id, to: dataAsset });
-              nodesLinks.push({ from: communicationLink.target, to: dataAsset });
-            })
-          }
-
-          if (showDataAssets && communicationLink.data_assets_received) {
-            communicationLink.data_assets_received.forEach((dataAsset) => {
-              nodesLinks.push({ from: technical_asset.id, to: dataAsset });
-              nodesLinks.push({ from: communicationLink.target, to: dataAsset });
-            })
-          }
+    let nodeDataArray = [];
+    if (showDataAssets) {
+      for (const daKey in yamlData.data_assets) {
+        if (yamlData.data_assets.hasOwnProperty(daKey)) {
+          const data_asset = yamlData.data_assets[daKey];
+          console.log(`${daKey}: ${data_asset}`);
+          nodeDataArray.push({ key: data_asset.id, threagile_model: data_asset, type: 'data_asset', caption: daKey, color: 'lightgreen' });
         }
       }
+    }
 
-      if (showDataAssets && technical_asset.data_assets_processed) {
-        technical_asset.data_assets_processed.forEach((dataAsset) => {
-          nodesLinks.push({ from: technical_asset.id, to: dataAsset });
-        });
-      }
+    let nodesLinks = [];
+    for (const taKey in yamlData.technical_assets) {
+      if (yamlData.technical_assets.hasOwnProperty(taKey)) {
+        const technical_asset = yamlData.technical_assets[taKey];
+        console.log(`${taKey}: ${technical_asset}`);
+        nodeDataArray.push({ key: technical_asset.id, threagile_model: technical_asset, type: 'technical_asset', caption: taKey, color: 'lightblue' });
 
-      if (showDataAssets && technical_asset.data_assets_stored) {
-        technical_asset.data_assets_stored.forEach((dataAsset) => {
-          nodesLinks.push({ from: technical_asset.id, to: dataAsset });
-        });
+        if (technical_asset.communication_links) {
+          for (const clKey in technical_asset.communication_links) {
+            const communicationLink = technical_asset.communication_links[clKey];
+            console.log(`${clKey}: ${communicationLink}`);
+            nodesLinks.push({ from: technical_asset.id, to: communicationLink.target });
+
+            if (showDataAssets && communicationLink.data_assets_sent) {
+              communicationLink.data_assets_sent.forEach((dataAsset) => {
+                nodesLinks.push({ from: technical_asset.id, to: dataAsset });
+                nodesLinks.push({ from: communicationLink.target, to: dataAsset });
+              })
+            }
+
+            if (showDataAssets && communicationLink.data_assets_received) {
+              communicationLink.data_assets_received.forEach((dataAsset) => {
+                nodesLinks.push({ from: technical_asset.id, to: dataAsset });
+                nodesLinks.push({ from: communicationLink.target, to: dataAsset });
+              })
+            }
+          }
+        }
+
+        if (showDataAssets && technical_asset.data_assets_processed) {
+          technical_asset.data_assets_processed.forEach((dataAsset) => {
+            nodesLinks.push({ from: technical_asset.id, to: dataAsset });
+          });
+        }
+
+        if (showDataAssets && technical_asset.data_assets_stored) {
+          technical_asset.data_assets_stored.forEach((dataAsset) => {
+            nodesLinks.push({ from: technical_asset.id, to: dataAsset });
+          });
+        }
       }
     }
+
+    myDiagram.model = new go.GraphLinksModel(nodeDataArray, nodesLinks);
+    openPropertyEditor(yamlData, 'projectInfo', schema.properties);
   }
 
-  myDiagram.model = new go.GraphLinksModel(nodeDataArray, nodesLinks);
-  openPropertyEditor(yamlData, 'projectInfo', schema.properties);
-}
-
-function restoreChanges() {
-  if (currentFile) {
-    const yamlData = jsyaml.load(currentFile);
-    console.log(yamlData);
-    updateDiagramModel(yamlData, document.getElementById('showDataAssetsCheckBox').checked);
+  function openPropertyEditor(nodeData, id, schema) {
+    const classEditor = new EditorGenerator(nodeData, schema, id);
+    // TODO: do not hard code hidden properties
+    classEditor.generateEditor(['communication_links', 'data_assets_processed', 'data_assets_stored',
+      'data_assets_sent', 'data_assets_received', 'data_assets', 'technical_assets',
+      'trust_boundaries', 'shared_runtimes', 'individual_risk_categories']);
   }
-}
-
-function exportDiagram() {
-  try {
-    const yamlData = jsyaml.dump(diagramYaml);
-    const blob = new Blob([yamlData], { type: 'text/yaml' });
-    const downloadLink = document.createElement('a');
-    downloadLink.href = URL.createObjectURL(blob);
-    downloadLink.download = 'diagram.yaml'; // Default file name
-    downloadLink.click();
-    URL.revokeObjectURL(downloadLink.href);
-  } catch (e) {
-    alert('Failed to export diagram.');
-    console.error("Error exporting diagram:", e);
-  }
-}
-
-function openPropertyEditor(nodeData, id, schema) {
-  const classEditor = new EditorGenerator(nodeData, schema, id);
-  // TODO: do not hard code hidden properties
-  classEditor.generateEditor(['communication_links', 'data_assets_processed', 'data_assets_stored',
-    'data_assets_sent', 'data_assets_received', 'data_assets', 'technical_assets',
-    'trust_boundaries', 'shared_runtimes', 'individual_risk_categories']);
-}
-
-window.addEventListener('DOMContentLoaded', init);
+});
